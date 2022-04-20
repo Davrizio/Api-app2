2404ed88713e236aa3c1b9a3aea403cd0501dbc6
Dave DeVries <davriziod@gmail.com>
2022-04-20 00:07:49 -0400

    starting to implement additional cards

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-//show how many cards each player has and how many left in the deck ADDD 2 to winning count...not just 1!
+//show how many cards each player has and how many left in the deck ADDD 2 to winning count...not just 1! DONE
 //player wins when they have all cards in deck
 //show additional cards and give winner cards during war
 //have war button show up when cards are the same
-//allow user to choose multiple decks with conditional so they don't choose too many (over 5 return "that's crazy!")
 
 
 
@@ -31,26 +30,54 @@ function getFetch(){
   
   const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
 
+
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+
+        let player1Card = data.cards[0].code
+        let player2Card = data.cards[1].code
         let val1 = Number(cardValue(data.cards[0].value))
         let val2 = Number(cardValue(data.cards[1].value))
+
         document.querySelector('#howManyCardsLeft').innerText = data.remaining
         document.querySelector('#player1').src = data.cards[0].image
         document.querySelector('#player2').src = data.cards[1].image
+        
         if(val1 > val2){
-          document.querySelector('h3').innerText = 'Player 1 WON!'
-          document.querySelector('#cards1').innerText = ++count1
+          document.querySelector('h3').innerText = 'Player 1 WON! that round'
+          count1 = count1 + 2
+          document.querySelector('#cards1').innerText = count1
         }else if(val1 < val2){
-          document.querySelector('h3').innerText = 'Player 2 WON!'
-          document.querySelector('#cards2').innerHTML = ++count2
+          document.querySelector('h3').innerText = 'Player 2 WON! that round'
+          count2 = count2 + 2
+          document.querySelector('#cards2').innerText = count2
         }else{
           document.querySelector('h3').innerText = 'WAR!'
         }
-      
-
+        fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/pile/player1/add/?cards=${player1Card}`)
+            .then(function (response){
+            if (response.ok){
+              return response.json();
+            }
+              return Promise.reject(response);
+            }).then(function (data){
+              console.log(data);
+           }).catch(function (error) {
+              console.warn('Something went wrong.', error);
+          });
+        fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/pile/player1/add/?cards=${player2Card}`)
+            .then(function (response){
+            if (response.ok){
+              return response.json();
+           }
+             return Promise.reject(response);
+            }).then(function (data){
+              console.log(data);
+           }).catch(function (error) {
+             console.warn('Something went wrong.', error);
+         });
       })
       .catch(err => {
           console.log(`error ${err}`)
@@ -70,3 +97,29 @@ function cardValue(val){
     return val
   }
 }
+
+/* function addToPile(){
+  
+  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/pile/player1/add/?cards=${player1Card()}`)
+  .then(function (response){
+  if (response.ok){
+    return response.json();
+  }
+    return Promise.reject(response);
+  }).then(function (data){
+    console.log(data);
+  }).catch(function (error) {
+    console.warn('Something went wrong.', error);
+});
+  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/pile/player1/add/?cards=${player2Card()}`)
+  .then(function (response){
+  if (response.ok){
+    return response.json();
+  }
+    return Promise.reject(response);
+  }).then(function (data){
+    console.log(data);
+  }).catch(function (error) {
+    console.warn('Something went wrong.', error);
+});
+} */
